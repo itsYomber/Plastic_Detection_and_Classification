9a0d840bb68dde09d2d1b73a72d6ac766708189e
xlFelipe11 <131472304+xlFelipe11@users.noreply.github.com>
2023-04-26 19:44:11 -0500

    Correción Tesis.py

--- TesisV2.py
+++ TesisV2.py
@@ -1,24 +1,29 @@
 import cv2
 import torch
 import Systems
+import serial
 
-model = torch.hub.load('ultralytics/yolov5','custom',path='D:/Universidad/Proyecto de grado/YoloDetec/model2/plastic_2.pt')
+#port = serial.Serial('COM9', 9600, timeout=1)
+model = torch.hub.load('ultralytics/yolov5','custom',path='D:/Universidad/Proyecto de grado/YoloDetec/model3/plastic_3.pt')
 
 #Captura del vídeo
-cap = []
-cap[0] = cv2.VideoCapture(0)
-cap[1] = cv2.VideoCapture(1)
+cap = [] ; window=False
+cap0 = cv2.VideoCapture(0)
+cap1 = cv2.VideoCapture(2)
 
 while True:
-    ret, frame = cap[0].read()
-    Plastico1 = Systems.System1_Funct(frame)
-    cv2.imshow("System 1",frame)
+    ret, crop = cap0.read()
+    Plastico1 = Systems.System1_Funct(crop)
+    cv2.imshow('system1',crop)
     if Plastico1 is True:
         print("Plastic Detected")
-        Systems.System2_Funct(cap[1],model,5)
+        #Systems.System2_Funct(window,cap1,port,model,10)
+    Systems.System2_Funct(window,cap1,model,Plastico1)
+    #port.write(b'')
     k = cv2.waitKey(1)
     if k==27:
         break
 cap[0].realese()
 cap[1].realese()
 cv2.destroyAllWindows
+#port.close()
