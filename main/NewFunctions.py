b5f3039b6b4d148e6a762d3452f5747869947f72
itsYomber <105079780+itsYomber@users.noreply.github.com>
2023-10-22 22:55:57 -0500

    Update NewFunctions.py (System1)

--- main/NewFunctions.py
+++ main/NewFunctions.py
@@ -1,18 +1,24 @@
+# Importar Librerias
 import numpy as np
 import cv2
 
+# Declaración de clase CircularBuffer
 class CircularBuffer:
+
+    # Inicialización de clase
     def __init__(self, size):
         self.size = size
         self.buffer = [None] * size
         self.head = 0  # Índice de la posición actual para insertar nuevos elementos
         self.is_full = False
-        
+
+    # Establecimiento de primer estado
     def start(self, item):
         self.buffer = [item] * self.size
         self.is_full = True
         self.head = 0
 
+    # Ingres de información a la clase
     def insert(self, item):
         self.buffer[self.head] = item
         self.head = (self.head + 1) % self.size  # Avanza al siguiente índice de forma circular
@@ -20,34 +26,52 @@ class CircularBuffer:
         if self.head == 0:
             self.is_full = True
 
+    # Extracción de elementos dentro de la clase
     def get_elements(self):
         if self.is_full:
             return self.buffer[self.head:] + self.buffer[:self.head]
         else:
             return self.buffer[:self.head]
 
-
+# Sistema de reconocimiento de botellas (captura de video, modelo Yolo, puerto serial)
 def System1(cap,model,port):
+
+    # Inicialización de Registro
     register = CircularBuffer(2)
     register.start(0)
+
+    # Bucle para captura de video
     while True:
         ret, frame = cap.read()
         detection = model(frame)
+
+        # Coordenadas y % de confianza de la detección
         coord = detection.xyxy[0].numpy()
         porcent = coord[:,4]
+        
+        # Selección de % de confianza mayor detectado en el caso de que se haya detectado mas de una botella
         if len(porcent)>1:
             maximo = max(porcent)
         else:
             maximo = porcent
+
+        # Ingreso de % de confianza en registro
         register.insert(maximo)
+
+        # Obtención de % de confianza promedio guardado en el registro
         elements = register.get_elements()
         prom = sum(elements)/len(elements)
+
+        # Si el promedio de % es mayor de 0.40 se asume que es una botella y se envía información serial
         if prom >= 0.40:
             port.write(b'B')
         else:
             port.write(b'b')
-            
-        cv2.imshow('Detector de Botellas', np.squeeze(detection.render()))  
+
+        # Visualización de lo que observa la captura de video con modelo Yolo aplicado
+        cv2.imshow('Detector de Botellas', np.squeeze(detection.render()))
+
+        # Asignación de Tecla de finalización del modelo (Esc)
         k = cv2.waitKey(1)
         if k==27:
             break
