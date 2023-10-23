412c57c72176899b5527a75bc52d17c12fe865b4
itsYomber <105079780+itsYomber@users.noreply.github.com>
2023-10-22 22:40:51 -0500

    Update Registros.py

--- AuxiliarCodes/Registros.py
+++ AuxiliarCodes/Registros.py
@@ -1,15 +1,20 @@
+# Declaración de clase CircularBuffer
 class CircularBuffer:
+
+    # Inicialización de clase
     def __init__(self, size):
         self.size = size
         self.buffer = [None] * size
         self.head = 0  # Índice de la posición actual para insertar nuevos elementos
         self.is_full = False
-        
+
+    # Establecimiento de primer estado
     def start(self, item):
         self.buffer = [item] * self.size
         self.is_full = True
         self.head = 0
 
+    # Ingres de información a la clase
     def insert(self, item):
         self.buffer[self.head] = item
         self.head = (self.head + 1) % self.size  # Avanza al siguiente índice de forma circular
@@ -17,6 +22,7 @@ class CircularBuffer:
         if self.head == 0:
             self.is_full = True
 
+    # Extracción de elementos dentro de la clase
     def get_elements(self):
         if self.is_full:
             return self.buffer[self.head:] + self.buffer[:self.head]
@@ -47,8 +53,7 @@ print(elementos)
 
 # El buffer está lleno, se sobrescribirá el primer elemento (Dato 1) al insertar uno nuevo
 buffer.insert(0)
-elementos = buffer.get_elements()
-print(elementos)
-
 
 # Obtener los elementos almacenados en el buffer
+elementos = buffer.get_elements()
+print(elementos)
